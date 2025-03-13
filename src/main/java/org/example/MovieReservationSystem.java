7527cef71a54a758ba8fb3a7bbe0f1226ff59b12
ranjitraje.cse@gmail.com <ranjitraje.cse@gmail.com>
2025-03-13 10:04:40 +0530

    initial commit

---
+++ src/main/java/org/example/MovieReservationSystem.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MovieReservationSystem {
+    private int availableSeats;
+    private final Lock reservationLock;
+
+    public MovieReservationSystem(int totalSeats) {
+        //initialises lock and the total seats available for reservation
+        this.availableSeats = totalSeats;
+        this.reservationLock = new ReentrantLock();
+    }
+
+
+    public boolean reserveSeats(int numSeats) {
+
+        try {
+            if(reservationLock.tryLock() && availableSeats >= numSeats) {
+                availableSeats -= numSeats;
+                return true;
+            }
+        } finally {
+            reservationLock.unlock();
+        }
+        return false;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+}
